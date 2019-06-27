8980c9a9971d43d4cc692ba662d6171fa64c3e57
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-06-27 16:28:04 +0100

    Add simple menu

---
+++ Assets/Scripts/UIFreeze.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFreeze : MonoBehaviour
+{
+    private Vector3 frozenPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frozenPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = frozenPos;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    [SerializeField] private Animator menuSlide;
+    private int currentMenu;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+
+            return;
+        }
+
+        instance = this;
+    }
+
+    private void Start()
+    {
+        currentMenu = 0;
+    }
+    public void MainMenu()
+    {
+        if (currentMenu == 0) return;
+        menuSlide.SetTrigger("Back");
+        currentMenu = 0;
+    }
+
+    public void SongPick()
+    {
+        if (currentMenu == 1) return;
+        menuSlide.SetTrigger("PickSongMenu");
+        currentMenu = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
