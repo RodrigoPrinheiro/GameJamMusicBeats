e7a0d86f503e0e8bb48d796449c79a3d6aca0742
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-06-28 16:47:59 +0100

    combo text

---
+++ Assets/Prefabs/ComboText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ComboText : MonoBehaviour
+{
+    public TextMeshPro text;
+
+    public void SetCombo(int combo)
+    {
+        text.text = combo.ToString();
+    }
+}
---
+++ Assets/Prefabs/ComboThing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboThing : MonoBehaviour
+{
+    public Metronome metronome;
+    public ComboText textprefab;
+    public Transform holder;
+    public float lifeTime = 5;
+
+
+    public void updateCombo()
+    {
+        if (!metronome)
+            return;
+
+        var txt = Instantiate(textprefab, holder);
+        txt.SetCombo(metronome.combo);
+        Destroy(txt.gameObject, lifeTime);
+    }
+}
